d837472c0e400d8524509344ee5edba4f4a55a00
Alain Huynh <alain.huynh86@gmail.com>
2021-09-22 02:24:44 +0200

    upload

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,12 +1,13 @@
-fetch('http://localhost:3000/api/teddies')
-  .then(function (res) {
-    if (res.ok) {
-      return res.json();
-    }
+// call API récupération des données
+fetch('http://localhost:3000/api/teddies/')
+  .then((response) => response.json())
+  .catch((error) => {
+    let messageError = document.querySelector(".cards");
+    messageError.innerHTML =
+      "Une erreur est survenue...";
+    messageError.style.textAlign = "center";
+    messageError.style.fontSize = "3em";
+    messageError.style.marginTop = "15rem";
   })
-  .then(function (value) {
-    console.log(value);
-  })
-  .catch(function (err) {
-    // Une erreur est survenue
-  });
+
+
