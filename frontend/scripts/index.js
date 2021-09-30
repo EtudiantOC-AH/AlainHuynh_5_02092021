f0cd10df035a28a7dfbfff8dcfa17e84ddfa8d0a
Alain Huynh <alain.huynh86@gmail.com>
2021-09-30 15:50:14 +0200

    update

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,13 +1,29 @@
-// call API récupération des données
-fetch('http://localhost:3000/api/teddies/')
+
+fetch('http://localhost:3000/api/teddies')
   .then((response) => response.json())
+  .then(teddies => {
+    console.log(teddies);
+    let teddiesHTML = ""
+    teddies.forEach(teddy => {
+      teddiesHTML += `
+      <div class="card">
+        <img class="img" src="${teddy.imageUrl}" />
+        <h3 class="name">${teddy.name}</h3>
+        <p class="price">${teddy.price / 100}€</p>
+        <a href="produit.html?id=${teddy.id}"> Voir le produit </a>
+      </div>
+    `
+    })
+    document.getElementById('productList').innerHTML = teddiesHTML
+  })
   .catch((error) => {
     let messageError = document.querySelector(".cards");
-    messageError.innerHTML =
-      "Une erreur est survenue...";
+    messageError.innerHTML = "Une erreur est survenue...";
     messageError.style.textAlign = "center";
     messageError.style.fontSize = "3em";
-    messageError.style.marginTop = "15rem";
+    messageError.style.display = "flex";
+    messageError.style.justifyContent = "center";
+    messageError.style.paddingBottom = '15rem';
+    messageError.style.paddingTop = '10rem';
   })
-
 
--- frontend/scripts/produit.js
+++ frontend/scripts/produit.js
@@ -1,2 +1,33 @@
-let params = new URL (document,location).searchParams;
-let id= params.get("id");
+let params = new URL(document, location).searchParams;
+let id = params.get("id");
+
+fetch('http://localhost:3000/api/teddies/${id}')
+  .then((response) => response.json())
+  .then(teddy => {
+    console.log(teddy);
+    let teddyHTML = ""
+    teddy.forEach(teddy => {
+      teddyHTML += `
+      <div class="card">
+        <img class="img" src="${teddy.imageUrl}" />
+        <h3 class="name">${teddy.name}</h3>
+        <p class="description">${teddy.description}</p>
+        <p class="colors">${teddy.colors}</p>
+        <p class="price">${teddy.price / 100}€</p>
+        <a href="panier.html?id=${teddy.id}"> Ajouter au panier </a>
+      </div>
+  `
+    })
+    document.getElementById('product').innerHTML = teddyHTML
+  })
+  .catch((error) => {
+    let messageError = document.querySelector(".cards");
+    messageError.innerHTML = "Une erreur est survenue...";
+    messageError.style.textAlign = "center";
+    messageError.style.fontSize = "3em";
+    messageError.style.paddingTop = "15rem";
+    messageError.style.paddingBottom = "15rem";
+  })
+
+
+
